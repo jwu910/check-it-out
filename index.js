a822570cf461e710ad9e3904afa4e0818f32eebe
Joshua Wu <Joshua.wu.0910@gmail.com>
2018-02-18 14:56:20 -0800

    Use blessed-contrib to build table

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const program = require('commander');
 const blessed = require('blessed');
+const contrib = require('blessed-contrib');
+const program = require('commander');
 
 program.version('0.0.1');
 
@@ -12,65 +13,40 @@ program.command('test').action(() => {
 
   screen.key(['escape', 'q', 'C-c'], (ch, key) => process.exit(0));
 
-
-  // Push each git branch to item array
-  const items = ['test2','test3'];
-
-  // Create a box perfectly centered horizontally and vertically.
-  const box = blessed.box({
-    top: 'center',
+  const table = contrib.table({
+    keys: true,
+    fg: 'white',
     left: 'center',
-    width: '80%',
+    top: 'center',
+    selectedFg: 'white',
+    selectedBg: 'blue',
+    interactive: true,
+    label: 'Check It Out',
+    width: '90%',
     height: '80%',
-    // Content could be another box? Maybe push each node into content to make selectable
-    content: items.join('\n'),
-    tags: true,
-    border: {
-      type: 'line'
-    },
-    style: {
-      fg: 'white',
-      border: {
-        fg: '#f0f0f0'
-      }
-    },
+    padding: 1,
+    border: { type: 'line', fg: 'magenta' },
+    columnSpacing: 3, //in chars
+    columnWidth: [80, 12, 12] /*in chars*/,
+  });
+
+
+
+  table.setData({
+    headers: ['Git Sha', 'col2', 'col3'],
+      // Data should be array of information. [remote, branch, date last commited, etc]
+      // Each data pushed to data array
+    data: [['THIS IS A TEST', 2, 3], ['This is a really really long test test test tests testst', 5, 6]],
   });
 
   // Append our box to the screen.
-  screen.append(box);
+  screen.append(table);
 
-  // Add a png icon to the box
-  // var icon = blessed.image({
-  //   parent: box,
-  //   top: 0,
-  //   left: 0,
-  //   type: 'overlay',
-  //   width: 'shrink',
-  //   height: 'shrink',
-  //   file: __dirname + '/my-program-icon.png',
-  //   search: false
-  // });
-
-  // If our box is clicked, change the content.
-  // box.on('click', function(data) {
-  //   box.setContent('{center}Some different {red-fg}content{/red-fg}.{/center}');
-  //   screen.render();
-  // });
-
-  // // If box is focused, handle `enter`/`return` and give us some more content.
-  // box.key('enter', function(ch, key) {
-  //   box.setContent('{right}Even different {black-fg}content{/black-fg}.{/right}\n');
-  //   box.setLine(1, 'bar');
-  //   box.insertLine(1, 'foo');
-  //   screen.render();
-  // });
-
-  // Focus our element.
-  box.focus();
+  //allow control the table with the keyboard
+  table.focus();
 
   // Render the screen.
   screen.render();
-
 });
 
 program.parse(process.argv);
