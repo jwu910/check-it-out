9237182881e14c57603dceb1a39439d6d098cabc
Joshua Wu <Joshua.wu.0910@gmail.com>
2018-02-18 15:48:46 -0800

    Update test data, require git functions

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const blessed = require('blessed');
 const contrib = require('blessed-contrib');
 const program = require('commander');
+const git = require('./utils/gitFunctions');
 
 program.version('0.0.1');
 
@@ -19,24 +20,21 @@ program.command('test').action(() => {
     left: 'center',
     top: 'center',
     selectedFg: 'white',
-    selectedBg: 'blue',
+    selectedBg: 'magenta',
     interactive: true,
     label: 'Check It Out',
     width: '90%',
-    height: '80%',
-    padding: 1,
+    height: '90%',
     border: { type: 'line', fg: 'magenta' },
-    columnSpacing: 3, //in chars
-    columnWidth: [80, 12, 12] /*in chars*/,
+    columnSpacing: 1, //in chars
+    columnWidth: [80, 20, 10] /*in chars*/,
   });
-
-
 
   table.setData({
     headers: ['Git Sha', 'col2', 'col3'],
       // Data should be array of information. [remote, branch, date last commited, etc]
       // Each data pushed to data array
-    data: [['THIS IS A TEST', 2, 3], ['This is a really really long test test test tests testst', 5, 6]],
+    data: [['THIS IS A TEST', 'Thello there are some words', 3], ['This is a really really long test test test tests testst', 5, 6]],
   });
 
   // Append our box to the screen.
