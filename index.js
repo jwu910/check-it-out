726aa6dcdd0246eedc044bced8ae1cd69379fca4
Joshua Wu <Joshua.wu.0910@gmail.com>
2018-02-18 14:26:14 -0800

    Begin development for blessed window

--- index.js
+++ index.js
@@ -1,13 +1,77 @@
 const program = require('commander');
+const blessed = require('blessed');
 
-program
-  .version('0.0.1')
+program.version('0.0.1');
 
-program
-  .command('test')
-  .action(() => {
-    console.log('Placeholder event START');
-  })
+program.command('test').action(() => {
+  const screen = blessed.screen({
+    autoPadding: true,
+    smartCSR: true,
+    title: 'Check It Out',
+  });
+
+  screen.key(['escape', 'q', 'C-c'], (ch, key) => process.exit(0));
+
+
+  // Push each git branch to item array
+  const items = ['test2','test3'];
+
+  // Create a box perfectly centered horizontally and vertically.
+  const box = blessed.box({
+    top: 'center',
+    left: 'center',
+    width: '80%',
+    height: '80%',
+    // Content could be another box? Maybe push each node into content to make selectable
+    content: items.join('\n'),
+    tags: true,
+    border: {
+      type: 'line'
+    },
+    style: {
+      fg: 'white',
+      border: {
+        fg: '#f0f0f0'
+      }
+    },
+  });
+
+  // Append our box to the screen.
+  screen.append(box);
+
+  // Add a png icon to the box
+  // var icon = blessed.image({
+  //   parent: box,
+  //   top: 0,
+  //   left: 0,
+  //   type: 'overlay',
+  //   width: 'shrink',
+  //   height: 'shrink',
+  //   file: __dirname + '/my-program-icon.png',
+  //   search: false
+  // });
+
+  // If our box is clicked, change the content.
+  // box.on('click', function(data) {
+  //   box.setContent('{center}Some different {red-fg}content{/red-fg}.{/center}');
+  //   screen.render();
+  // });
+
+  // // If box is focused, handle `enter`/`return` and give us some more content.
+  // box.key('enter', function(ch, key) {
+  //   box.setContent('{right}Even different {black-fg}content{/black-fg}.{/right}\n');
+  //   box.setLine(1, 'bar');
+  //   box.insertLine(1, 'foo');
+  //   screen.render();
+  // });
+
+  // Focus our element.
+  box.focus();
+
+  // Render the screen.
+  screen.render();
+
+});
 
 program.parse(process.argv);
 
