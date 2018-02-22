502a88be237e73c2a8d66dc703e613d78e019c02
Joshua Wu <Joshua.wu.0910@gmail.com>
2018-02-21 22:05:12 -0800

    Refactor index.js to use listtable instead of blessed-contrib

--- index.js
+++ index.js
@@ -1,46 +1,50 @@
 const blessed = require('blessed');
-const contrib = require('blessed-contrib');
 const program = require('commander');
-const git = require('./utils/gitFunctions');
+const git = require('./utils/git');
 
 program.version('0.0.1');
 
 program.command('test').action(() => {
   const screen = blessed.screen({
     autoPadding: true,
+    debug: true,
+    fullUnicode: true,
     smartCSR: true,
     title: 'Check It Out',
+    warnings: true,
   });
 
   const currBranch = git.currentBranch();
-  const listData = git.getBranchList();
 
   screen.key(['escape', 'q', 'C-c'], (ch, key) => process.exit(0));
 
-  const table = contrib.table({
-    keys: true,
-    fg: 'white',
+  const table = blessed.listtable({
+    align: 'left',
+    border: { type: 'line' },
+    height: 'shrink',
+    top: 'top',
     left: 'center',
-    top: 'center',
-    selectedFg: 'white',
-    selectedBg: 'magenta',
-    interactive: true,
-    label: 'Check It Out -- Current Branch: ' + currBranch,
-    width: '90%',
-    height: '90%',
-    border: { type: 'line', fg: 'magenta' },
-    columnSpacing: 1, //in chars
-    columnWidth: [
-      60, 10
-    ] /*in chars TODO: Get char length as percentage(or set width) of output*/,
-  });
-
-  table.setData({
-    // set data object
-    headers: ['Branch Name', 'Remote'],
-    // Data should be array of information. [remote, branch, date last commited, etc]
-    // Each data pushed to data array
-    data: [['test'], ['test']],
+    keys: true,
+    noCellBorders: true,
+    style: {
+      border: { fg: '#EF9B66' },
+      cell: {
+        selected: {
+          bg: '#FFFFFF',
+          fg: '#000000',
+        },
+      },
+      header: {
+        fg: '#EF9B66',
+      },
+      label: {
+        fg: '#66D9EF',
+      },
+    },
+    tags: true,
+    top: 1,
+    vi: true,
+    width: '70%',
   });
 
   // Append our box to the screen.
@@ -49,8 +53,22 @@ program.command('test').action(() => {
   //allow control the table with the keyboard
   table.focus();
 
-  // Render the screen.
-  screen.render();
+  table.on('select', (val, key) => {
+    return process.exit(0);
+  });
+
+  git.buildListArray().then(results => {
+    const listData = results;
+    
+    table.setData(
+      [
+        ['Branch Name', 'Remote'],
+        ...listData
+      ]
+    );
+
+    screen.render();
+  });
 });
 
 program.parse(process.argv);
