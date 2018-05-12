3eb856e97fe8343e851e3bbd02a80af9a34e0df6
Joshua Wu <Joshua.wu.0910@gmail.com>
2018-05-12 15:10:45 -0700

    CIO-65 Add gulp task runner

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var babel = require('gulp-babel');
+
+gulp.task('build', function() {
+  return gulp
+    .src('src/**/*.js')
+    .pipe(babel())
+    .pipe(gulp.dest('lib'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('src/**/*.js', ['build']);
+})
