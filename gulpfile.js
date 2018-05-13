8f91fc9956805ab2fd648594748220d6ead9bcc4
Joshua Wu <joshua.wu.0910@gmail.com>
2018-05-12 20:53:57 -0700

    CIO-65 Broken Promises (#105)
    * CIO-65 Update unhandled error catchall. Begin rewriting promises
    
    * CIO-65 Remove unecessary condition from prompt
    
    * CIO-65 Remove async function. Rewrite refreshTable with better promise use
    
    * CIO-65 Remove async await from git utils
    
    * CIO-65 Remove async await from utils
    
    * CIO-65 SF
    
    * CIO-65 Move getRemoteTabs into promise resolution
    
    * CIO-65 Refactor select promise logic and formatting
    
    * CIO-65 Return execGit as promise.
    
    * CIO-65 SF
    
    * CIO-65 Update documentation and formatting
    
    * CIO-65 Add error handle function
    
    * CIO-65 Handle detatched head method
    
    * CIO-65 add type constants for success messages
    
    * CIO-65 Add create constant
    
    * CIO-65 Move confirmation question data into handleDetatchedHead
    
    * CIO-65 Add dev dependencies
    
    * CIO-65 Remove create branch on remote
    
    * CIO-65 Remove create branch export
    
    * CIO-65 Update git checkout options
    
    * CIO-65 SF
    
    * CIO-65 Add babel
    
    * CIO-65 Add babelrc
    
    * CIO-65 Add src dir for babel
    
    * CIO-65 Add version flag check
    
    * CIO-65 Add lib dir for prod files
    
    * CIO-65 Update git promise
    
    * CIO-65 Change vars to let with introduction of babel
    
    * CIO-65 Destructure git methods
    
    * CIO-65 Remove handle success
    
    * CIO-65 Update Git promise
    
    * CIO-65 Rebuild
    
    * CIO-65 Update index js to remove anti pipe msg
    
    * CIO-65 appjs SF
    
    * CIO-65 SF
    
    * CIO-65 Update babel presets
    
    * CIO-65 Add reject to refresh. Update docheckout method. Change getRefs to getRefsArray
    
    * CIO-65 Add gulp task runner
    
    * CIO-65 Update gulp file
    
    * CIO-65 Move readError to utils. Change exports to es6
    
    * CIO-65 Update git utils to es6 exports. Update readError method
    
    * CIO-65 Update readerror
    
    * CIO-65 Update build and format list promises
    
    * CIO-65 Rebuild
    
    * CIO-65 Update gulpfile err logs
    
    * CIO-65 Update selected line assignments
    
    * CIO-65 Rebuild
    
    * CIO-65 SF
    
    * CIO-65 Rebuild
    
    * CIO-65 Update npm watch
    
    * CIO-65 Cannot refresh error
    
    * CIO-65 Rebuild

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var babel = require('gulp-babel');
+var chalk = require('chalk');
+var gulp = require('gulp');
+
+gulp.task('build', function() {
+  return gulp
+    .src('src/**/*.js')
+    .pipe(babel())
+    .on('error', function (error) {
+      process.stderr.write(chalk.red.bold('Build failed.') + '\n');
+      process.stderr.write(error.fileName + '\n');
+      process.stderr.write(error.stack + '\n');
+
+      this.emit('end');
+    })
+    .pipe(gulp.dest('lib'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('src/**/*.js', ['build']);
+})
