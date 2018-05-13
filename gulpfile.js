f65cf952aade134be58b6ddd1bdd41520edb8ad8
Joshua Wu <Joshua.wu.0910@gmail.com>
2018-05-12 17:57:48 -0700

    CIO-65 Update gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,18 @@
-var gulp = require('gulp');
 var babel = require('gulp-babel');
+var chalk = require('chalk');
+var gulp = require('gulp');
 
 gulp.task('build', function() {
   return gulp
     .src('src/**/*.js')
     .pipe(babel())
+    .on('error', function (error) {
+      console.log(chalk.red.bold('Build failed.'));
+      console.log(error.fileName);
+      console.log(error.stack);
+
+      this.emit('end');
+    })
     .pipe(gulp.dest('lib'));
 });
 
