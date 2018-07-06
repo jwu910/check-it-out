ef0a91529ed9b0a3c8ec41a0e00dbfb9557e2f67
Joshua Wu <joshua.wu.0910@gmail.com>
2018-07-06 09:33:17 -0700

    CIO-194 Test PR (#209)
    * CIO-194 Install danger
    
    * CIO-194 Autogenerate packagelock
    
    * CIO-194 Add danger script
    
    * CIO-194 Initial commit of danger file
    
    * CIO-194 Update danger file imports.
    
    * CIO-194 Update imports
    
    * CIO-194 Install lodash
    
    * CIO-194 Copy changes from example danger files
    
    * CIO-194 Copy more settings
    
    * CIO-194 Comment out package.json test for now.
    
    * CIO-194 Update warning for package-lock
    
    * CIO-194 Add Assignee rule to danger
    
    * CIO-194 Update pr msg body rule and update pr asignee
    
    * CIO-194 Autogenerate package-lock
    
    * CIO-194 Run yarn install. Autogenerate yarn.lock
    
    * CIO-194 Add yarn install
    
    * CIO-194 Bump required node version to 6.

---
+++ dangerfile.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const includes = require('lodash.includes');
+
+const { danger, fail, markdown, message, warn } = require('danger');
+
+const jsModifiedFiles = danger.git.modified_files.filter(
+  path => path.startsWith('src') && path.endsWith('js'),
+);
+
+const hasAppChanges =
+  jsModifiedFiles.filter(filepath => !filepath.endsWith('test.js')).length > 0;
+
+const prBodyMsg = danger.github.pr.body;
+
+// Fails if the description is too short.
+if (!prBodyMsg || prBodyMsg.length < 8 || prBodyMsg.indexOf('Fixes #')) {
+  fail(
+    ':grey_question: This pull request needs a description. \n' +
+      'Please include "Fixes #<ISSUE_NUMBER>". \n' +
+      'See <a href="https://github.com/jwu910/check-it-out/blob/master/CONTRIBUTING.md">Contributing Guidelines</a>',
+  );
+}
+
+// Warns if package lock was not updated.
+const packageChanged = includes(danger.git.modified_files, 'package.json');
+const lockfileChanged = includes(
+  danger.git.modified_files,
+  'package-lock.json',
+);
+if (packageChanged && !lockfileChanged) {
+  const message =
+    'Changes were made to package.json, but not to package-lock.json';
+  const idea =
+    'Perhaps you need to run `npm install`? \n' +
+    'Package Lock files can be commited with  the message `CIO-#<ISSUE_NUMBER> Autogenerate package-lock`';
+  warn(`${message} - <i>${idea}</i>`);
+}
+
+// Fail if there are app changes without a CHANGELOG
+if (!danger.git.modified_files.includes('CHANGELOG.md') && hasAppChanges) {
+  const changelogLink =
+    'https://github.com/jwu910/check-it-out/blob/master/CHANGELOG.md';
+  fail(
+    `Please include a CHANGELOG entry. You can find it at <a href='${changelogLink}'>CHANGELOG.md</a>`,
+  );
+}
+
+// Always ensure we assign someone, so that our Slackbot can do its work correctly
+if (danger.github.pr.assignee === null) {
+  fail(
+    'Please assign someone to merge this PR, and optionally include people who should review.',
+  );
+}
