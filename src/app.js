db6941e50200a0cc735044e1d8f148c34df2d963
Joshua Wu <Joshua.wu.0910@gmail.com>
2019-02-13 10:43:56 -0800

    CIO-0 SF

--- src/app.js
+++ src/app.js
@@ -21,10 +21,17 @@ const pkg = require(path.resolve(__dirname, '../package.json'));
 const notifier = updateNotifier({ pkg });
 
 if (notifier.update) {
-
   const notifierMessage = `\
-  New ${chalk.yellow(notifier.update.type)} version of ${notifier.update.name} available ${chalk.red(notifier.update.current)} ➜  ${chalk.green(notifier.update.latest)}\
-  \n${chalk.yellow('Changelog:')} ${chalk.blue(`https://github.com/jwu910/check-it-out/releases/tag/v${notifier.update.latest}`)}\
+  New ${chalk.yellow(notifier.update.type)} version of ${
+    notifier.update.name
+  } available ${chalk.red(notifier.update.current)} ➜  ${chalk.green(
+    notifier.update.latest,
+  )}\
+  \n${chalk.yellow('Changelog:')} ${chalk.blue(
+    `https://github.com/jwu910/check-it-out/releases/tag/v${
+      notifier.update.latest
+    }`,
+  )}\
   \nRun ${chalk.green('npm i -g check-it-out')} to update!`;
 
   notifier.notify({ message: notifierMessage });
