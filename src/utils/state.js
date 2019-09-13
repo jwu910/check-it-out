efdb8344855f1b724f155f9a2cba54c64b7bb6a3
drewbrokke <drew.brokke@liferay.com>
2019-09-13 14:44:17 -0500

    fix(ap.js, state.js): handle RegEx errors and moves state to its own file
    provide default values for each RegEx in case the given search and filters throw errors. We can also
    use accessors on the state object to prevent accidentally trying to set values directly to a
    computed property. Instead, use setters to make the access points for updating the state explicit.
    This also allows us to compute properties as needed.

---
+++ src/utils/state.js
@@ -0,0 +1,116 @@
+/**
+ * @typedef {import('../app').Ref} Ref
+ * @typedef {import('../app').Remote} Remote
+ */
+
+const defaultFilterRegex = new RegExp('');
+const defaultSearchRegex = new RegExp('^$');
+
+export const getState = logger => {
+  // State variables
+  const stateObject = {
+    currentRemoteIndex: 0,
+    currentRemote: null,
+    filter: '',
+    filterRegex: defaultFilterRegex,
+    search: '',
+    searchHits: [],
+    searchRegex: defaultSearchRegex,
+    remotes: [],
+  };
+
+  return {
+    /** @type {Remote} */
+    get currentRemote() {
+      return stateObject.currentRemote;
+    },
+
+    /** @type {number} */
+    get currentRemoteIndex() {
+      return stateObject.currentRemoteIndex;
+    },
+
+    /** @type {string} */
+    get filter() {
+      return stateObject.filter;
+    },
+
+    /** @type {RegExp} */
+    get filterRegex() {
+      return stateObject.filterRegex;
+    },
+
+    /** @type {Remote[]} */
+    get remotes() {
+      return stateObject.remotes;
+    },
+
+    /** @type {string} */
+    get search() {
+      return stateObject.search;
+    },
+
+    /** @type {number[]} */
+    get searchHits() {
+      return stateObject.searchHits;
+    },
+
+    /** @type {RegExp} */
+    get searchRegex() {
+      return stateObject.searchRegex;
+    },
+
+    /** @type {(number) => void} */
+    setCurrentRemoteIndex(newIndex) {
+      stateObject.currentRemoteIndex = newIndex;
+      stateObject.currentRemote = stateObject.remotes[newIndex];
+    },
+
+    /** @type {(string) => void} */
+    setFilter(newFilter) {
+      stateObject.filter = newFilter;
+
+      try {
+        stateObject.filterRegex = new RegExp(newFilter, 'gi');
+      } catch (error) {
+        logger.error(error.message);
+
+        stateObject.filterRegex = defaultFilterRegex;
+      }
+    },
+
+    /** @type {(string) => void} */
+    setSearch(newSearch) {
+      stateObject.search = newSearch;
+
+      try {
+        stateObject.searchRegex = new RegExp(newSearch, 'gi');
+      } catch (error) {
+        logger.error(error.message);
+
+        stateObject.searchRegex = defaultFilterRegex;
+      }
+
+      if (!newSearch) {
+        stateObject.searchHits = [];
+        return;
+      }
+
+      stateObject.searchHits = stateObject.currentRemote.refs
+        .filter(ref => ref.name.search(stateObject.filterRegex) !== -1)
+        .map((ref, index) => {
+          if (ref.name.search(stateObject.searchRegex) !== -1) {
+            return index + 1;
+          }
+
+          return NaN;
+        })
+        .filter(index => !isNaN(index));
+    },
+
+    /** @type {(newRemotes: Remote[]) => void} */
+    setRemotes(newRemotes) {
+      stateObject.remotes = newRemotes;
+    },
+  };
+};
